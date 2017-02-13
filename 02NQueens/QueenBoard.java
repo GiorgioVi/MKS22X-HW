893792625260c23b7ae266d7a9afcde87beee173
Giorgio Vidali <gvidali@stuy.edu>
2017-02-13 14:47:56 -0500

    show some class

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -3,9 +3,27 @@ public class QueenBoard{
   private int solutionCount;
 
   public QueenBoard(int size){
- board = new int[size][size];
-   }
-
+ 	 board = new int[size][size];
+	 fillWith0s();
+  }
+  
+  public void fillWith0s(){
+	for(int x = 0; x < board.length; x++){
+		for(int y = 0; y < board.length; y++){
+			board[x][y] = 0;
+		}
+	}
+}
+ public String toString(){
+	String visualBoard = "";
+	for(int x = 0; x < board.length; x++){
+		for(int y = 0; y < board.length; y++){
+			visualBoard += board[x][y] + " ";
+		}
+			visualBoard += "\n";
+	}
+	return visualBoard;
+}
    /**
     *precondition: board is filled with 0's only.
     *@return false when the board is not solveable. true otherwise.
@@ -35,7 +53,12 @@ public class QueenBoard{
     *and all nunbers that represent queens are replaced with 'Q'
     *all others are displayed as underscores '_'
     */
-   public String toString(){
-     return "";
-   }
+
+
+ public static void main(String[] arghhhh){
+	QueenBoard tester = new QueenBoard(8);
+	System.out.println(tester.toString());
+}
+
+
 }
