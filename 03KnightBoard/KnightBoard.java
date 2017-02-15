428a65494a9d3622247f143e00205181725dd36d
Giorgio Vidali <gvidali@stuy.edu>
2017-02-15 14:44:48 -0500

    in the works

---
+++ 03KnightBoard/KnightBoard.java
@@ -0,0 +1,42 @@
+public class KnightBoard{
+	
+	private int[][] board;
+
+	public KnightBoard(int startingRow, int startingCol, int size){
+		board = new int[size][size];
+		fillWith0s();
+		board[startingRow][startingCol] = 1;
+	}
+
+	public void fillWith0s(){
+		for(int x = 0; x < board.length; x++){
+			for(int y = 0; y < board.length; y++){
+				board[x][y] = 0;
+			}
+		}
+	}
+
+	public String toString(){
+		String visualBoard = "";
+		for(int x = 0; x < board.length; x++){
+			for(int y = 0; y < board.length; y++){
+				if(board[x][y] < 10){
+					visualBoard += " _" + board[x][y];
+				}else{
+					visualBoard += " " + board[x][y];
+				}		
+			}
+				visualBoard += "\n";	
+		}
+		return visualBoard;
+	}
+	
+
+
+
+	public static void main(String[] arghhhh){
+		KnightBoard cyclone = new KnightBoard(3, 2, 8);
+		System.out.println(cyclone.toString());
+	}
+
+}
