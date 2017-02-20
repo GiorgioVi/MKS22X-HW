3e97af50ef6dab5454ced3ecd9021ac6c464e228
Giorgio Vidali <gvidali@stuy.edu>
2017-02-19 22:18:41 -0500

    works on most

--- 03KnightBoard/KnightBoard.java
+++ 03KnightBoard/KnightBoard.java
@@ -1,16 +1,15 @@
 public class KnightBoard{
-	
+
 	private int[][] board;
 
-	public KnightBoard(int startingRow, int startingCol, int size){
-		board = new int[size][size];
+	public KnightBoard(int startingRow, int startingCol){
+		board = new int[startingRow][startingCol];
 		fillWith0s();
-		board[startingRow][startingCol] = 1;
 	}
 
 	public void fillWith0s(){
 		for(int x = 0; x < board.length; x++){
-			for(int y = 0; y < board.length; y++){
+			for(int y = 0; y < board[0].length; y++){
 				board[x][y] = 0;
 			}
 		}
@@ -19,27 +18,68 @@ public class KnightBoard{
 	public String toString(){
 		String visualBoard = "";
 		for(int x = 0; x < board.length; x++){
-			for(int y = 0; y < board.length; y++){
+			for(int y = 0; y < board[0].length; y++){
 				if(board[x][y] < 10){
 					visualBoard += " _" + board[x][y];
 				}else{
 					visualBoard += " " + board[x][y];
-				}		
+				}
 			}
-				visualBoard += "\n";	
+				visualBoard += "\n";
 		}
 		return visualBoard;
 	}
-	
+
 	public boolean solveH(int row, int col, int level){
-			
-	}
+		if(level > board.length * board[0].length){
+			return true;
+		}
+		if(row > -1 && row < board.length &&
+		col > -1 && col < board[0].length
+		&& board[row][col] == 0){
+		board[row][col] = level;
+		level++;
+		if(solveH(row + 1, col + 2, level)){
+	 	return true;
+			}
+		if(solveH(row - 1, col + 2, level)){
+	 	return true;
+			}
+		if(solveH(row + 1, col - 2, level)){
+	 	return true;
+			}
+		if(solveH(row - 1, col - 2, level)){
+	 	return true;
+			}
+		if(solveH(row + 2, col + 1, level)){
+	 	return true;
+	 		}
+		if(solveH(row - 2, col + 1, level)){
+	 	return true;
+			}
+		if(solveH(row + 2, col - 1, level)){
+	 	return true;
+			}
+		if(solveH(row - 2, col - 1, level)){
+	 	return true;
+			}
+		board[row][col] = 0;
+			}
+		return false;
+}
 
 	public void solve(){
-
+		for(int x = 0; x < board.length; x++){
+			for(int y = 0; y < board[0].length; y++){
+				if(solveH(x, y, 1)){
+					break;
+				}
+			}
+		}
 	}
 	public static void main(String[] arghhhh){
-		KnightBoard cyclone = new KnightBoard(3, 2, 8);
+		KnightBoard cyclone = new KnightBoard(6, 6);
+		cyclone.solve();
 		System.out.println(cyclone.toString());
 	}
 
