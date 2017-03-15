eabd763c2aa1b47bb09d7e58f4c1a9d7a73fbd64
Giorgio Vidali <gvidali@stuy.edu>
2017-03-15 14:49:14 -0400

    great work

---
+++ 07Quick/Quick.java
@@ -0,0 +1,18 @@
+public class Quick{
+
+	public static int quickselect(int[] data, int k){
+			
+	}
+
+	private static int part(int[] data, int start, int end){
+	
+	}
+
+	public static void main(String[] arghhhhhhh){
+        	int[] pizza = {5,1,12,35,43,65,43,1,2,45};
+		System.out.println(quickselect(pizza,1));	
+
+}
+
+
+}
