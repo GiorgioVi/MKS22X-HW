a99e8101c5a3b0c70426a981c38c976852812ce9
Giorgio Vidali <gvidali@stuy.edu>
2017-03-23 14:48:05 -0400

    started merge

---
+++ 08Merge/Merge.java
@@ -0,0 +1,16 @@
+public class Merge{
+
+	public static void mergesort(int[] ary){
+	
+	
+
+	}
+
+  	public void merge(int[]a, int[]b, int[]destination){
+		for(int i = 0; i < destination.length; i++){
+			int aInd = 0;
+			int bInd = 0;
+			while
+
+	}
+}
