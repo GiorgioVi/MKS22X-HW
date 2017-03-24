a6ed8b6f96b16e1b7ad53585d5643f3ccee3feb4
Giorgio Vidali <gvidali@stuy.edu>
2017-03-24 11:48:18 -0400

    dfgd

--- 08Merge/Merge.java
+++ 08Merge/Merge.java
@@ -8,9 +8,28 @@ public class Merge{
 
   	public void merge(int[]a, int[]b, int[]destination){
 		for(int i = 0; i < destination.length; i++){
-			int aInd = 0;
-			int bInd = 0;
-			while
+			int aI = 0;
+			int bI = 0;
+			while(aInd < a.length && bind < b.length){
+				if(a[aI] <= b[bI]){
+					destination[i] = a[aI];
+					aI++;
+				}
+				else{
+					desitantion = b[bI];
+					bi++;
+				}
+
+
+	public static void main(String[] arghhhhh){
+	int[] a = {1,3,5,7,9};
+	int[] b = {1,2,4,7};
+	-
+	
+	}
 
 	}
+
+
+
 }
