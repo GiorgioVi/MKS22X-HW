4f8f7708c08d581a5317f753291c078fb6aa3e87
Giorgio Vidali <gvidali@stuy.edu>
2017-03-28 14:47:33 -0400

    much confusion

---
+++ 09LinkedList/MyLinkedList.java
@@ -0,0 +1,35 @@
+public class MyLinkedList{
+	private LNode start;
+	private int size;
+   
+  
+    private class LNode{
+	private int element;
+	private LNode nextNode;
+
+	private LNode(int ele, Lnode nex){
+	element = ele;
+	nextNode = nexl;
+	}
+    }
+	
+	public MyLinkedList(){
+	size = 0;
+	}
+	
+	public MyLinked
+	
+	public int getSize(){
+	return size;
+	}
+
+	public string toString(){
+		if(start == null) return "[]";
+		String body = "[";	
+		for(int i = 0; i < size; i ++){
+			body+=
+	}
+
+
+
+}
