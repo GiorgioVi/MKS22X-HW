f1ba488c0ee9bc925e6d74062bab113f38827d1e
Giorgio Vidali <gvidali@stuy.edu>
2017-04-04 14:46:03 -0400

    A long way to go

--- 09LinkedList/MyLinkedList.java
+++ 09LinkedList/MyLinkedList.java
@@ -1,35 +1,82 @@
 public class MyLinkedList{
-	private LNode start;
-	private int size;
    
   
     private class LNode{
-	private int element;
-	private LNode nextNode;
-
-	private LNode(int ele, Lnode nex){
-	element = ele;
-	nextNode = nexl;
+	LNode next,prev;
+	int value;
+	public LNode(int ele){
+		value = ele;
 	}
+	
+	public String toString(){
+		return value + "";
+    	}
     }
 	
+
+    LNode head,tail;
+    int size;
+	
 	public MyLinkedList(){
-	size = 0;
+	}
+
+	public int size(){
+		return size;
 	}
 	
-	public MyLinked
+        private LNode getNthNode(int n){
+	/*complete this*/
+	return head;
+	}
+
+        private void addAfter(LNode location, LNode toBeAdded){
+	/*complete this*/        
+	}
+
+        private void remove(LNode target){
+	/*complete this*/
+	}
+
+        public String toString(){
+		String body = "[";
+		LNode current = head;
+		while(current != null){
+			body += (current.value + ", ");
+		}
+		return "]" + body;	
+	}
+
+        public boolean add(int value){  
+	/*complete this*/
+	return true;
+	}
+
+        public int get(int index){
+	/*complete this*/
+	return 6;
+	}
+
+        public int set(int index, int value){
+	/*complete this*/
+	return 6;
+	}
+
+        public int indexOf(int value){
+	/*complete this*/
+	return 7;
+	}
+
+        public int remove(int index){
+	/*complete this*/
+	return 7;	
+	}
+
+        public void add(int index,int value){
+	/*complete this*/
+	}
 	
-	public int getSize(){
-	return size;
+   	public static void main(String[] argggg){
+	
+
 	}
-
-	public string toString(){
-		if(start == null) return "[]";
-		String body = "[";	
-		for(int i = 0; i < size; i ++){
-			body+=
-	}
-
-
-
 }
