3ab77ca10f4e21af20f074e064c2b383e46326c0
Giorgio Vidali <gvidali@stuy.edu>
2017-04-04 22:25:30 -0400

    swell

--- 09LinkedList/MyLinkedList.java
+++ 09LinkedList/MyLinkedList.java
@@ -1,82 +1,104 @@
 public class MyLinkedList{
-   
-  
+
+
     private class LNode{
-	LNode next,prev;
-	int value;
-	public LNode(int ele){
-		value = ele;
-	}
-	
-	public String toString(){
-		return value + "";
-    	}
-    }
-	
+	     LNode next,prev;
+	     int value;
+	       public LNode(int ele){
+		         value = ele;
+         }
+         private LNode(int ele, LNode nextN){
+           value = ele;
+           next = nextN;
+         }
+
+	        public String toString(){
+		         return value + "";
+    	   }
+     }
+
 
     LNode head,tail;
     int size;
-	
+
 	public MyLinkedList(){
+    size = 0;
 	}
 
 	public int size(){
 		return size;
 	}
-	
-        private LNode getNthNode(int n){
+
+  private LNode getNthNode(int n){
 	/*complete this*/
 	return head;
 	}
 
-        private void addAfter(LNode location, LNode toBeAdded){
-	/*complete this*/        
-	}
-
-        private void remove(LNode target){
+  private void addAfter(LNode location, LNode toBeAdded){
 	/*complete this*/
 	}
 
-        public String toString(){
+  private void remove(LNode target){
+	/*complete this*/
+	}
+
+  public String toString(){
 		String body = "[";
 		LNode current = head;
 		while(current != null){
 			body += (current.value + ", ");
+      current = current.next;
 		}
-		return "]" + body;	
+		return body + "]";
 	}
 
-        public boolean add(int value){  
-	/*complete this*/
-	return true;
-	}
+  private boolean add(int value){
+    if(size() == 0){
+      LNode beta = new LNode(value);
+      head = beta;
+    }else{
+     LNode current = head;
+     while(current.next != null){
+        current = current.next;
+     }
+     LNode beta  =  new LNode(value);
+     current.next = beta;
+     beta.prev = current;
+     this.tail = beta;
+    }
+    this.size += 1;
+    return true;
+   }
 
-        public int get(int index){
+  public int get(int index){
 	/*complete this*/
 	return 6;
 	}
 
-        public int set(int index, int value){
+  public int set(int index, int value){
 	/*complete this*/
 	return 6;
 	}
 
-        public int indexOf(int value){
+  public int indexOf(int value){
 	/*complete this*/
 	return 7;
 	}
 
-        public int remove(int index){
+  public int remove(int index){
 	/*complete this*/
-	return 7;	
+	return 7;
 	}
 
-        public void add(int index,int value){
+  public void add(int index,int value){
 	/*complete this*/
 	}
-	
+
    	public static void main(String[] argggg){
-	
+      MyLinkedList pizza = new MyLinkedList();
+      pizza.add(1);
+      System.out.println(pizza.toString());
+
 
 	}
 }
