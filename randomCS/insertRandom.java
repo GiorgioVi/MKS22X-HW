55e1cb1cc763a1b7042739616cad9ba293fc586c
Giorgio Vidali <gvidali@stuy.edu>
2017-05-01 14:48:04 -0400

    ap practice

---
+++ randomCS/insertRandom.java
@@ -0,0 +1,27 @@
+public class insertRandom{
+
+
+public static String toString(int[][] data){
+String x = "";
+for(int i = 0; i < data.length; i++){
+	for(int j = 0; j < data[0].length; j++){
+	x += data[i][j];
+}x += "\n";}
+return x;
+}
+
+public static String fillRandom(int[] number){
+int[][] data = new int[3][3];
+String x = "";
+return x;
+}
+
+
+
+public static void main(String[] arghhhh){
+int[][] a = {{1,2,3},{4,5,6},{7,8,9}};
+System.out.println(toString(a));
+}
+
+
+}
