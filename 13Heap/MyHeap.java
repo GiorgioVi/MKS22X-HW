58291eb5eec7d47da4513b7841d25af6c0c2adf3
Giorgio Vidali <gvidali@stuy.edu>
2017-05-04 14:11:06 -0400

    p

---
+++ 13Heap/MyHeap.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class MyHeap{
+   private int size;
+   private ArrayList<String> data;
+ 
+	public MyHeap(){
+	}
+
+	public MyHeap(boolean beta){
+		if(beta){
+
+		}else{
+
+		}
+	}
+
+	public void add(String s){
+	}
+	
+	public String remove(){
+		return "";
+	}
+
+	public String peek(){
+		return "";
+	}
+
+	private void pushUp(){
+	}
+
+	private void pushDown(){
+	}
+
+	public static void main(String[] argggg){
+		MyHeap pizza = new MyHeap();
+
+
+	}	
+}
