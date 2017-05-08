48a5591bf811217be7fbcc5b94db6568d4a4c4f7
Giorgio Vidali <gvidali@stuy.edu>
2017-05-08 02:50:51 -0400

    b

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -1,47 +1,98 @@
-import java.util.ArrayList;
+import java.util.*;
+import java.util.NoSuchElementException;
 
 public class MyHeap{
+   private String[] data = new String[10];
+   private int max = 1;
    private int size;
-   private ArrayList<String> data;
-   private boolean max;
- 
+
 	public MyHeap(){
+  size = 0;
 	}
 
 	public MyHeap(boolean beta){
-		size = data.size();
+  size = 0;
 		if(beta){
-			max = true;
+			max = 1;
 		}else{
-			max = false;
+			max = -1;
 		}
 	}
+  private void getBig(){
+    String[]big = new String[size*2];
+    for (int i =0;i <= size;i++ ) {
+      big[i] = data[i];
+    }
+    data = big;
+  }
+
+  public void swap(int a, int b){
+	   String feta = data[b];
+	   data[b] = data[a];
+	   data[a] = feta;
+    }
 
 	public void add(String s){
-		if(size != 1) data.add(
+  if(size+1 == data.length) getBig();
+  size++;
+  data[size] = s;
+  pushUp(size);
+
 	}
-	
+
 	public String remove(){
-		return "";
+		if(size == 0) throw new NoSuchElementException();
+    String x = data[1];
+    data[1] = data[size];
+    size--;
+    pushDown(1);
+    return x;
 	}
 
 	public String peek(){
-	if(size == 1){
-		return null;
+	if(size == 0){
+		throw new NoSuchElementException();
 	}else{
-		return data.get(1);
+		return data[1];
 	}
 	}
 
-	private void pushUp(){
+  public boolean comp(String first, String sec){
+	if(max >0){
+	    return first.compareTo(sec) > 0;
+	}else{
+	    return sec.compareTo(first) > 0;
 	}
+  }
 
-	private void pushDown(){
+  private void pushUp(int k){
+  if (k > 1 && comp(data[k],data[k/2])){
+     swap(k, k/2);
+     pushUp(k/2);
+  }
+  }
+
+  private void pushDown(int k){
+	if(k*2+1 <= size){
+	    if(comp(data[k*2], data[k*2+1])){
+		      swap(k, k*2);
+		        pushDown(k*2);
+	    }else{
+		      swap(k, k*2+1);
+		        pushDown(k*2+1);
+	    }
+	}else{
+	    if (k * 2 <= size){
+		      swap(k, k*2);
+	    }
+	    }
+  }
+
+  public String toString(){
+	String x = "[";
+	for (int i = 1;i <= size;i++){
+	    x+=data[i] + ", "
 	}
-
-	public static void main(String[] argggg){
-		MyHeap pizza = new MyHeap();
-
-
-	}	
+	return x + "]";
+    }
 }
