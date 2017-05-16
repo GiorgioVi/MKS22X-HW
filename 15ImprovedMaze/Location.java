a3e68c215a1e5e3d534628e369b9554aa5032727
Giorgio Vidali <gvidali@stuy.edu>
2017-05-16 14:49:39 -0400

    gfd

---
+++ 15ImprovedMaze/Location.java
@@ -0,0 +1,10 @@
+public class Location implements Comparable<Location>{
+  private int col, row, distanceToStart;
+  private Location prev;
+
+  public Location(int c, int r, int dts, Location p){
+  }
+
+  public int compareTo(Location other){
+  }
+}
